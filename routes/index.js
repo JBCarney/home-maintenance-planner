9b55a0a4e9c9fca2c530bbae036ef94e60e1777b
Jason Carney <jason@jbcarney.com>
2017-06-25 15:47:27 -0400

    Fix typo on hmpCtrl

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var standupCtrl = require('../controllers/hmp.server.controller');
+var hmpCtrl = require('../controllers/hmp.server.controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  return standupCtrl.list(req, res);
+  return hmpCtrl.list(req, res);
 });
 
 /* POST filter by member name - home page. */
 router.post('/', function(req, res) {
-    return standupCtrl.filterByMember(req, res);
+    return hmpCtrl.filterByMember(req, res);
 });
 
 /* GET New Note page. */
 router.get('/newtask', function(req, res) {
-    return standupCtrl.getNote(req, res);
+    return hmpCtrl.getNote(req, res);
 });
 
 /* POST New Note page. */
 router.post('/newtask', function(req, res) {
-    return standupCtrl.create(req, res);
+    return hmpCtrl.create(req, res);
 });
 
 module.exports = router;
